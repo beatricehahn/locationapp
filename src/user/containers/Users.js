b46751f5fa59b91a73f24eb608b99819c356c8ee
Beatrice Hahn <beatricehahn7@gmail.com>
2023-04-27 19:11:00 -0700

    fi placeholder image url

--- src/user/containers/Users.js
+++ src/user/containers/Users.js
@@ -3,7 +3,12 @@ import React from 'react';
 import UsersList from '../components/UsersList';
 
 const Users = () => {
-    const USERS=[{id:'001', name: 'Jane Doe', image: 'none', places:2}];
+    const USERS=[{
+        id:'001', 
+        name: 'Jane Doe',
+        image: 'https://www.petlandtexas.com/wp-content/uploads/2016/08/Red_Bunny_Petland_Puppy.jpg', 
+        places:2
+    }];
     return <UsersList items={USERS} />
 }
 
