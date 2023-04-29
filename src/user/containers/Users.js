da5e1737474200b4968870a9bf064fc0f329b1bc
Beatrice Hahn <beatricehahn7@gmail.com>
2023-04-28 20:33:27 -0700

    Refactor organization of users object

--- src/user/containers/Users.js
+++ src/user/containers/Users.js
@@ -1,14 +1,15 @@
-// import react
 import React from 'react';
 import UsersList from '../components/UsersList';
 
 const Users = () => {
-    const USERS=[{
-        id:'001', 
-        name: 'Jane Doe',
-        image: 'https://www.petlandtexas.com/wp-content/uploads/2016/08/Red_Bunny_Petland_Puppy.jpg', 
-        places:2
-    }];
+    const USERS = [
+        {
+            id:'001', 
+            name: 'Jane Doe',
+            image: 'https://www.petlandtexas.com/wp-content/uploads/2016/08/Red_Bunny_Petland_Puppy.jpg', 
+            places: 2
+        }
+    ];
     return <UsersList items={USERS} />
 }
 
